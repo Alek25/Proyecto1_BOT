0587c8e4fcea24576fdb27e25ff2a14911f06761
Alek25 <diaza913@gmail.com>
2017-04-30 19:57:30 -0600

    Programación inicial
    Se inicia la escritura del programa, archivo inicial
    
    Ventana principal, con titulo y tamaño definido

---
+++ Principal.py
@@ -0,0 +1,17 @@
+# Proyecto #1 de Taller de Programación
+# Autor: Alejandro Díaz Pereira. Carné: 2017172898
+
+from tkinter import *
+
+principal = Tk()
+
+principal.minsize(width=900, height=500)
+principal.title("C.I.P.H.E.R")
+
+
+
+
+
+
+
+principal.mainloop()
