b3625dc7d6eb61cba439f62b98a12a5ef8558632
Alek25 <diaza913@gmail.com>
2017-04-30 23:29:06 -0600

    Añade widgets (4)
    Los widgets añadidos son:
    	Lista Comandos
    	Espacio para escoger
    	Botón para confirmar
    	Consola de respuestas
    	Label con instrucción al usuario("Escoja un número...")

--- Principal.py
+++ Principal.py
@@ -2,11 +2,52 @@
 # Autor: Alejandro Díaz Pereira. Carné: 2017172898
 
 from tkinter import *
+import tkinter.ttk as ttk
 
+# Crea la ventana principal
 principal = Tk()
 
-principal.minsize(width=900, height=500)
+principal.minsize(width=900, height=450)
 principal.title("C.I.P.H.E.R")
+
+# Coloca lista de comandos
+comandos = open("Comandos.txt", "r").read()
+
+lista_comandos = Label(principal,
+                       text=comandos,
+                       height=15,
+                       width=25,
+                       relief="ridge",
+                       font="DejaVu_Sans_Mono",
+                       justify=LEFT,
+                       anchor="w")
+lista_comandos.grid(row=1, column=1)
+
+# Crea campo para escribir número de comando
+L_comandos = Label(text="Escriba un número de comando (1, 2, 3...)")
+L_comandos.grid(row=2,column=1)
+
+choose_comandos = Entry(principal, bg="white", width=20)
+choose_comandos.insert(0, ">>> ")
+choose_comandos.grid(row=3, column=1, sticky="W")
+
+# Detecta activación de la tecla Enter, entradas: event(tecla presionada), salidas: ninguna
+def detect_enter(event):
+    choose_comandos.delete(0, END)
+    choose_comandos.insert(0, ">>> ")
+
+exec_comandos = Button(text="Ejecutar", command=detect_enter)
+exec_comandos.grid(row=3, column=1, sticky="E")
+
+
+choose_comandos.bind("<Return>", detect_enter)
+
+consola = Text(principal, width=90, height=11, bg="white", relief="sunken")
+consola.grid(row=2, column=2, rowspan=10)
+
+
+
+
 
 
 
