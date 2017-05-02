afc3bcd97da4ab9baa431f35b59b689f837eb005
Alek25 <diaza913@gmail.com>
2017-05-02 10:08:23 -0600

    Se añade ventana que solicita nombre de usuario.
    Se añadió una venta que solicita el nombre de usuario.
    Esto con el fin de que la interacción con el bot sea más amigable.
    
    El nombre digitado en el mensaje será el prompt utilizado
    en la entrada de comandos.

--- Principal.py
+++ Principal.py
@@ -5,31 +5,55 @@ from tkinter import *
 import tkinter.ttk as ttk, json
 
 file_data = open("data.txt")
+read_data = file_data.read()
+data = json.loads(read_data)
+
 
 # Crea la ventana principal
 principal = Tk()
 principal.minsize(width=783, height=450)
 principal.title("C.I.P.H.E.R")
 
+comando = StringVar()
+choose_comando = Entry(principal, bg="white", width=22, textvariable=comando)
+choose_comando.grid(row=3, column=1, sticky="W")
+
 ventana_usuario = Toplevel(principal)
 ventana_usuario.title("Nombre de usuario")
 ventana_usuario.geometry("400x80")
 ventana_usuario.wm_transient(principal)
-#ventana_usuario.wm_transient(principal)
 
-var_prompt = ""
 var_nombre = StringVar()
+var_comando = ""
+
 # Pedir nombre de usuario
+# Entradas: ign (dtos ignorados)
+# Salidas: Ninguna
 def nombre_usuario(ign):
     if var_nombre.get() == "":
-        global var_prompt
-        var_prompt = "Invitado: "
-        print(var_prompt)
+        global comando, var_comando
+        comando.set("Invitado: ")
+        var_comando = comando.get()
+        ventana_usuario.destroy()
     else:
-        var_prompt = (var_nombre.get() + ": ")
+        comando.set(var_nombre.get() + ": ")
+        var_comando = comando.get()
+        ventana_usuario.destroy()
+
+# Impide que el prompt sea borrado.
+# Entradas: ign, ign2, ign3
+# (estas variables existen para que la función se ejecute correctamente, no son usadas en ningún momento).
+# Salidas: Ninguna
+def prompt(ign, ing2, ing3):
+    global var_comando
+    if var_comando not in comando.get():
+        choose_comando.insert(END, " ")
+        choose_comando.icursor(END)
+comando.trace_variable("w", prompt)
+
 
 instruccion1 = Label(ventana_usuario, text="Escriba su nombre")
-instruccion2 = Label(ventana_usuario, text="(Déjelo en blanco para usar el nombre predeterminado")
+instruccion2 = Label(ventana_usuario, text="(Déjelo en blanco para usar el nombre predeterminado)")
 entrada_usuario = Entry(ventana_usuario, width=45, bg="white", textvariable=var_nombre)
 instruccion1.pack()
 instruccion2.pack()
@@ -55,34 +79,13 @@ lista_comandos.grid(row=1, column=1)
 L_comandos = Label(text="Escriba un número de comando (1, 2, 3...)")
 L_comandos.grid(row=2, column=1)
 
-comando = StringVar()
-choose_comando = Entry(principal, bg="white", width=20, textvariable=comando)
-
-print(comando.set(var_prompt))
-choose_comando.grid(row=3, column=1, sticky="W")
-
-# Impide que el prompt sea borrado.
-# Entradas: ign, ign2, ign3
-# (estas variables existen para que la función se ejecute correctamente, no son usadas en ningún momento).
-# Salidas: Ninguna
-def prompt(ign, ing2, ing3):
-    var = comando.get()
-    if ">>> " not in var:
-        choose_comando.insert(3, " ")
-        choose_comando.icursor(4)
-#comando.trace_variable("w", prompt)
-
-
-exec_comandos = Button(text="Ejecutar")
+exec_comandos = Button(text="Enviar")
 exec_comandos.grid(row=3, column=1, sticky="E")
 
 consola = Text(principal, width=73, height=11, bg="white", relief="sunken")
 consola.grid(row=2, column=2, rowspan=10)
 
-
-energia = file_data.read()
-
-power = json.loads(energia)["Energia"]
+power = data["Energia"]
 barra_energia = ttk.Progressbar(orient="horizontal", mode="determinate", value=power, length=480)
 barra_energia.place(x=264, y=7)
 
@@ -100,8 +103,5 @@ sector5 = cuadricula.create_rectangle(4, 128, 128, 256, width=2)
 sector6 = cuadricula.create_rectangle(128, 128, 256, 256, width=2)
 sector7 = cuadricula.create_rectangle(256, 128, 384, 256, width=2)
 sector8 = cuadricula.create_rectangle(384, 128, 512, 256, width=2)
-
-imagen = PhotoImage(file="multimedia/bill.gif")
-imagen1 = cuadricula.create_image(64, 64, image=imagen, anchor="center")
 
 principal.mainloop()
