3d2ea9dc6a84fc669e7b294536ddeb1e7e66ee21
Alek25 <diaza913@gmail.com>
2017-05-02 09:31:05 -0600

    Please enter the commit message for your changes. Lines starting

--- Principal.py
+++ Principal.py
@@ -4,10 +4,38 @@
 from tkinter import *
 import tkinter.ttk as ttk, json
 
+file_data = open("data.txt")
+
 # Crea la ventana principal
 principal = Tk()
 principal.minsize(width=783, height=450)
 principal.title("C.I.P.H.E.R")
+
+ventana_usuario = Toplevel(principal)
+ventana_usuario.title("Nombre de usuario")
+ventana_usuario.geometry("400x80")
+ventana_usuario.wm_transient(principal)
+#ventana_usuario.wm_transient(principal)
+
+var_prompt = ""
+var_nombre = StringVar()
+# Pedir nombre de usuario
+def nombre_usuario(ign):
+    if var_nombre.get() == "":
+        global var_prompt
+        var_prompt = "Invitado: "
+        print(var_prompt)
+    else:
+        var_prompt = (var_nombre.get() + ": ")
+
+instruccion1 = Label(ventana_usuario, text="Escriba su nombre")
+instruccion2 = Label(ventana_usuario, text="(Déjelo en blanco para usar el nombre predeterminado")
+entrada_usuario = Entry(ventana_usuario, width=45, bg="white", textvariable=var_nombre)
+instruccion1.pack()
+instruccion2.pack()
+entrada_usuario.place(x=18, y=50)
+
+entrada_usuario.bind("<Return>", nombre_usuario)
 
 
 # Coloca lista de comandos
@@ -29,7 +57,8 @@ L_comandos.grid(row=2, column=1)
 
 comando = StringVar()
 choose_comando = Entry(principal, bg="white", width=20, textvariable=comando)
-choose_comando.insert(0, ">>> ")
+
+print(comando.set(var_prompt))
 choose_comando.grid(row=3, column=1, sticky="W")
 
 # Impide que el prompt sea borrado.
@@ -41,7 +70,7 @@ def prompt(ign, ing2, ing3):
     if ">>> " not in var:
         choose_comando.insert(3, " ")
         choose_comando.icursor(4)
-comando.trace_variable("w", prompt)
+#comando.trace_variable("w", prompt)
 
 
 exec_comandos = Button(text="Ejecutar")
@@ -51,8 +80,7 @@ consola = Text(principal, width=73, height=11, bg="white", relief="sunken")
 consola.grid(row=2, column=2, rowspan=10)
 
 
-file_energia = open("data.txt")
-energia = file_energia.read()
+energia = file_data.read()
 
 power = json.loads(energia)["Energia"]
 barra_energia = ttk.Progressbar(orient="horizontal", mode="determinate", value=power, length=480)
@@ -72,5 +100,8 @@ sector5 = cuadricula.create_rectangle(4, 128, 128, 256, width=2)
 sector6 = cuadricula.create_rectangle(128, 128, 256, 256, width=2)
 sector7 = cuadricula.create_rectangle(256, 128, 384, 256, width=2)
 sector8 = cuadricula.create_rectangle(384, 128, 512, 256, width=2)
+
+imagen = PhotoImage(file="multimedia/bill.gif")
+imagen1 = cuadricula.create_image(64, 64, image=imagen, anchor="center")
 
 principal.mainloop()
